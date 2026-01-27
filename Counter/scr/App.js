fcca39c87f4588f17fa9b212baafdd6012a2e728
RanvijaySuryawanshi <ranvijaysuryawanshi@gmail.com>
2026-01-27 10:55:14 +0530

    1

---
+++ Counter/scr/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets Start Counting!!!</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
