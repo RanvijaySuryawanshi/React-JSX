fe08e322a5a1f77b6366fe36494bbfce9c53a98f
RanvijaySuryawanshi <ranvijaysuryawanshi@gmail.com>
2026-02-05 15:40:54 +0530

    1

---
+++ React+Bootstrap/scr/App.js
@@ -0,0 +1,17 @@
+import Navbar from "./Components/Navbar";
+import HeroCarousel from "./Components/HeroCarousel";
+import Cards from "./Components/Cards";
+import MyModal from "./Components/MyModal";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroCarousel />
+      <Cards />
+      <MyModal />
+    </>
+  );
+}
+
+export default App;
